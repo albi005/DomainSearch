f301da0bfef4a440f28568170b472119adf1c163
Albert Ragány-Németh <raganyalbi@gmail.com>
2023-01-21 00:01:14 +0100

    Add empty Windows only MAUI app

---
+++ DomainSearch/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainSearch;
+
+public partial class MainPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ DomainSearch/MauiProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace DomainSearch;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp() => MauiApp.CreateBuilder()
+            .UseMauiApp<App>()
+            .Build();
+}
+
+public class App : Application
+{
+    public App()
+    {
+        MainPage = new MainPage();
+    }
+}
